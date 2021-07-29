7ce1e1103e51f9f1dbc246b6f96fff7ce1518f50
Abel Palomino <abelpr87@gmail.com>
2021-07-29 17:48:25 -0500

    Example XMLHttpRequest

--- apr.WebMVC/Models/ResponseResult.cs
+++ apr.WebMVC/Models/ResponseResult.cs
@@ -1,23 +1,1 @@
-﻿using System.Collections.Generic;
-
-namespace apr.WebMVC.Models
-{
-    public class ResponseResult<T>
-    {
-        private bool _state;
-        public bool State
-        {
-            get
-            {
-                return (Items != null || Item != null) || _state;
-            }
-            set
-            {
-                _state = value;
-            }
-        }
-        public string Message { get; set; }
-        public T Item { get; set; }
-        public List<T> Items { get; set; }
-    }
-}
+﻿using System.Collections.Generic;  namespace apr.WebMVC.Models {     public class ResponseResult<T>     {         private bool _state;         public bool State         {             get             {                 return (Items != null || Item != null) || _state;             }             set             {                 _state = value;             }         }         public string Message { get; set; }         public T Item { get; set; }         public List<T> Items { get; set; }     } }
--- apr.WebMVC/assets/app/js/ExampleAjax/XMLHttpRequest.js
+++ apr.WebMVC/assets/app/js/ExampleAjax/XMLHttpRequest.js
@@ -1,1 +1,47 @@
-﻿
+﻿let urlBaseApi = 'http://localhost:54330';
+let url = `${urlBaseApi}/Categoria/findAll`;
+
+//Variables
+const tblCategorias = document.getElementById('tblCategorias');
+
+//Metodos
+const getCategorias = () => {
+    let req = new XMLHttpRequest();
+
+    req.open('GET', url);
+
+    req.onload = evt => {
+        // console.log(req.responseText);
+        let data = JSON.parse(evt.target.responseText);
+        //console.log(data);
+        draw(data.Items);
+    };
+
+    req.onerror = evt => {
+        console.log(evt.target);
+    }
+
+    req.send(null);
+}
+
+const draw = (items)=> {
+
+    const tableBody = tblCategorias.querySelector('tbody');
+    let htmlRows = '';
+
+    tableBody.innerHTML =  '';
+
+    items.forEach(cat => {
+        htmlRows += 
+        `<tr>
+            <td>${cat.IdCategoria}</td>
+            <td>${cat.Nombre}</td>
+            <td>${cat.Descripcion == null ? '' : cat.Descripcion}</td>
+        </tr>`;
+    });
+
+    tableBody.innerHTML = htmlRows;
+}
+
+//Ejecucion de los metodos
+getCategorias();
