00e00139cd55d5bfa4fa3c4d4d64e7cc21860445
Abel Palomino <abelpr87@gmail.com>
2021-07-29 18:17:16 -0500

    Example Fetch Asyc Await

--- apr.WebMVC/assets/app/js/ExampleAjax/FetchAPIAsync.js
+++ apr.WebMVC/assets/app/js/ExampleAjax/FetchAPIAsync.js
@@ -1,1 +1,37 @@
-﻿
+﻿let urlBaseApi = 'http://localhost:54330';
+let url = `${urlBaseApi}/Categoria/findAll`;
+
+//Variables
+const tblCategorias = document.getElementById('tblCategorias');
+
+//Metodos
+const getCategorias = async () => {
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    //console.log(data);
+    draw(data.Items);
+}
+
+const draw = (items)=> {
+
+    const tableBody = tblCategorias.querySelector('tbody');
+    let htmlRows = '';
+
+    tableBody.innerHTML =  '';
+
+    items.forEach(cat => {
+        htmlRows += 
+        `<tr>
+            <td>${cat.IdCategoria}</td>
+            <td>${cat.Nombre}</td>
+            <td>${cat.Descripcion == null ? '' : cat.Descripcion}</td>
+        </tr>`;
+    });
+
+    tableBody.innerHTML = htmlRows;
+}
+
+//Ejecucion
+getCategorias();
